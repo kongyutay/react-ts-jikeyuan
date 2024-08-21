45e6fdce25f94ba1682aa30e8a827e3109596691
Tay Kong Yu <kongyutay@gmail.com>
2024-08-21 22:02:16 +0800

    setup basic router

---
+++ src/router/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Detail from '../pages/Detail'
+import Home from '../pages/Home'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/detail',
+        element: <Detail />
+    }
+])
+
+export { router }
