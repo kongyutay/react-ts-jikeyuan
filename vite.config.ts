06666885ad9a2d2af053d627dcd0a715820de0ae
Tay Kong Yu <kongyutay@gmail.com>
2024-08-21 22:46:17 +0800

    set @ as src file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
+//ts 的话要安装类型声明文件 npm i @types/node -D
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname, './src')
+    }
+  }
 })
