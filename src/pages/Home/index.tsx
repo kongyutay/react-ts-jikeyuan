a290996d4256d322cc59b9d58c6501039413eba7
Tay Kong Yu <kongyutay@gmail.com>
2024-08-22 21:31:25 +0800

    home module - fetch data and render tab

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,35 @@
+import './style.css'
+import { Tabs } from 'antd-mobile'
+import {fetchChannelAPI, type ChannelItem } from '@/apis/list'
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-    return <div>this is Home</div>
+    const [channels, setChannels] = useState<ChannelItem[]>([]);
+    useEffect(()=> {
+        const getChannels = async () => {
+            try {
+                const res = await fetchChannelAPI();
+                setChannels(res.data.data.channels);
+            } catch (error) {
+                throw new Error('Fetch channel error');
+            }
+        }
+        getChannels();
+    }, [])
+    return (
+        <div>
+            <div className='tabContainer'>
+                <Tabs>
+                    {channels.map(item => (
+                        <Tabs.Tab title={item.name} key={item.id}>
+                            菠萝
+                        </Tabs.Tab>
+                    ))}
+                    
+                </Tabs>
+            </div>
+        </div>
+    )
 }
 
 export default Home
